bd7999421aba59d09a72f336ec532a01b06878e0
abhishekrahulcs <abhishekrahulcs@gmail.com>
2023-05-24 09:25:28 +0530

    dcoker

---
+++ V2/api-gateway/src/test/java/com/inpractice/microservices/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inpractice.microservices.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ V2/currency-exchange-service/src/main/java/com/inpractice/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -0,0 +1,32 @@
+package com.inpractice.microservices.currencyexchangeservice;
+
+
+import org.slf4j.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
+
+
+
+@RestController
+public class CircuitBreakerController {
+   
+	private Logger logger=LoggerFactory.getLogger(CircuitBreakerController.class);
+	
+	@GetMapping("/sample-api")
+	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+	public String sampleApi() {
+		logger.info("Sample api call received");
+		ResponseEntity<String> forEntity=new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+		
+		return forEntity.getBody();
+		//return "sample-Api";
+	}
+	
+	public String hardcodedResponse(Exception exp) {
+		return "fallback-response";	
+	}
+}
